c32c912a57f2dec5024346daaac0e3baa512e97c
ariel-mora <ariel.mora@epn.edu.ec>
2023-05-16 15:37:22 -0500

    MODO ANIMAL

--- src/Animal.java
+++ src/Animal.java
@@ -1,5 +1,10 @@
 public class Animal {
     public Animal (){
         System.out.println("Soy animal");
+        asfjkhahsfhjkas
+        aksjhfjkasfhjkasjkhfaskjkfhas
+        asfjkhahsfhjkas
+
+        
     }
 }
