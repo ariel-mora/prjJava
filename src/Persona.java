eac2ff2f3e9564fd0bf4046681045bbd1e127d40
ariel-mora <ariel.mora@epn.edu.ec>
2023-05-17 15:03:20 -0500

    Hotfix Ok

--- src/Persona.java
+++ src/Persona.java
@@ -1,6 +1,5 @@
 public class Persona {
     public Persona (){
         System.out.println("I am a nice person");
-        System.out.println("I have a  issue ");
     }     
 }
--- src/SuperAnimal.java
+++
@@ -1,6 +0,0 @@
-public class SuperAnimal {
-    public SuperAnimal () {
-        System.out.println("Soy animal ");
-        System.out.println("I am Buugg ");
-    }   
-}
